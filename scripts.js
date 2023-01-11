f124307a83b3e34a0cc6a47ebf753972114aca7b
DevSuelen <117091949+DevSuelen@users.noreply.github.com>
2023-01-11 10:03:34 -0300

    aula 2 de programação

---
+++ scripts.js
@@ -0,0 +1,46 @@
+// document.getElementById("container"). innerHtml = "conteudo";
+// document.getElementById("container"). innerHtml = "conteudo";
+
+let container = document.getElementById("container");
+
+// let macas = 2;
+// let banana =3;
+// let soma = macas / banana;
+
+
+// container.innerHTML=soma;
+
+// let texto = "Meu nome é";
+// let nome = "Suelen Magalhães";
+
+// let soma = texto = nome;
+
+// container. innerHTML= soma;
+
+// const NASCIMENTO = "08/02/1986";
+// let nome = "Suelen Magalhães ";
+
+
+
+
+// // container. innerHTML= NASCIMENTO;
+
+// // NASCIMENTO = "08/02/1986";
+
+// container.innerHTML = NASCIMENTO;
+
+// container.innerHTML = nome + NASCIMENTO;
+
+function somar ()
+{
+    let v1 = Number(valor1.value);
+    let v2= Number(valor2.value);
+    document.getElementById("resultado").innerHTML=v1+v2;
+
+}
+function subtrair ()
+{
+    let v1 = Number(valor1.valor2);
+    let v2= Number(valor2.value);
+    document.getElementById("resultado").innerHTML=v1-v2;
+}
